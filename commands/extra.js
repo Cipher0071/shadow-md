078bdfd167e16eb84792d887395a26e4bff1e06e
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-17 05:03:06 +0100

    Update extra.js

--- commands/extra.js
+++ commands/extra.js
@@ -1,1 +1,23 @@
+const axios = require('axios');
+const { tlang, tiny, runtime, formatp, botpic, prefix, sck1 } = require("../lib");
+const fetch = require('node-fetch');
+const Shadow = require('../lib/commands');
 
+Shadow.cmd({
+    pattern: "animequote",
+    desc: "Sends random anime quotes in chat.",
+    kingclass: "anime",
+    kingpath: __filename,
+},
+    async (Void, citel, text) => {
+        try {
+            const res = await fetch('https://some-random-api.com/animu/quote');
+            if (!res.ok) throw await res.text();
+            const json = await res.json();
+            const { sentence, character, anime } = json;
+            const message = `📜𝐐𝐔𝐎𝐓𝐄:\n${sentence}\n\n👤𝐂𝐇𝐀𝐑𝐀𝐂𝐓𝐄𝐑: \`\`\`${character}\`\`\`\n🌟𝐀𝐍𝐈𝐌𝐄: \`\`\`${anime}\`\`\`\n`;
+            await Void.sendMessage(citel.chat, { text: message }, 'extendedTextMessage', { quoted: citel });
+        } catch (error) {
+            console.error(error);
+        }
+    });
