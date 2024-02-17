8e6fd2131adc83805c06864596fb56cf2dde9ed8
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-17 06:48:05 +0100

    Update guessflag_.js

--- commands/guessflag_.js
+++ commands/guessflag_.js
@@ -10,11 +10,11 @@ cmd({
     desc: 'Starts a guessing game',
     category: 'game',
 }, async (Void, citel, text) => {
-    conn.tebakbendera = conn.tebakbendera || {};
+    citel.tebakbendera = citel.tebakbendera || {};
 
     let id = citel.chat;
-    if (id in conn.tebakbendera) {
-        await citel.reply(citel.chat, 'There are still unanswered questions in this chat', conn.tebakbendera[id][0]);
+    if (id in citel.tebakbendera) {
+        await citel.reply(citel.chat, 'There are still unanswered questions in this chat', citel.tebakbendera[id][0]);
         throw false;
     }
 
@@ -26,13 +26,13 @@ Use the command ${prefix}answer to answer the question
 Use the command ${prefix}hint for a hint
     `.trim();
 
-    conn.tebakbendera[id] = [
+    citel.tebakbendera[id] = [
         await Void.sendFile(citel.chat, json.img, '', caption, citel),
         json,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) {
-                citel.reply(citel.chat, `Time's up!\nThe answer is *${json.name}*`, conn.tebakbendera[id][0]);
-                delete conn.tebakbendera[id];
+            if (citel.tebakbendera[id]) {
+                citel.reply(citel.chat, `Time's up!\nThe answer is *${json.name}*`, citel.tebakbendera[id][0]);
+                delete citel.tebakbendera[id];
             }
         }, timeout)
     ];
@@ -46,11 +46,11 @@ cmd({
 }, async (Void, citel, text) => {
     let id = citel.chat;
 
-    conn.tebakbendera = conn.tebakbendera || {};
+    citel.tebakbendera = citel.tebakbendera || {};
     
-    if (!(id in conn.tebakbendera)) throw false;
+    if (!(id in citel.tebakbendera)) throw false;
     
-    let json = conn.tebakbendera[id][1];
+    let json = citel.tebakbendera[id][1];
     
     if (text.trim().toLowerCase() == json.name.toLowerCase().trim()) {
         await citel.reply(citel.chat, '✅ *Correct!*', citel);
@@ -58,7 +58,7 @@ cmd({
         await citel.reply(citel.chat, '❌ *Wrong!*', citel);
     }
 
-    delete conn.tebakbendera[id];
+    delete citel.tebakbendera[id];
 });
 
 // Function to get a hint during the guessing game
@@ -69,11 +69,11 @@ cmd({
 }, async (Void, citel) => {
     let id = citel.chat;
 
-    conn.tebakbendera = conn.tebakbendera || {};
+    citel.tebakbendera = citel.tebakbendera || {};
     
-    if (!(id in conn.tebakbendera)) throw false;
+    if (!(id in citel.tebakbendera)) throw false;
     
-    let json = conn.tebakbendera[id][1];
+    let json = citel.tebakbendera[id][1];
     
     await citel.reply(citel.chat, '```' + json.name.replace(/[AIUEOaiueo]/ig, '_') + '```', citel);
 });
