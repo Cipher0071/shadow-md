afa043a1726a28e534d25a839803ab42968b2e61
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-17 05:24:12 +0100

    Update and rename switch-deact.js to deact.js

--- commands/deact.js
+++ commands/deact.js
@@ -1,14 +1,13 @@
 /**
- Copyright (C) 2022.
+ Copyright (C) 2024.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+ * @project_name : Shadow-Md
+ * @author : Cipher0071 <https://github.com/Cipher0071>
+ * @description : Shadow,A Multi-functional whatsapp bot.
+ * @version 0.0.1
  **/
-
  const { sck,sck1,cmd, getAdmin, tlang, prefix } = require('../lib')
  const Config = require('../config')
  
