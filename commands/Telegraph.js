df1641cc1da58664cc56f6978201f05ea39888a6
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-02-17 02:12:37 +0100

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -30,7 +30,7 @@ cmd({
         
         // Trim the text and remove the prefix
         const regex = new RegExp(`^${prefix}\\s*`, 'i');
-        y = a.trim().replace(regex, "");
+       let y = a.trim().replace(regex, "");
 
         let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Shadow+Bot&author_name=Cipher0071&content=[%7B"tag":"p","children":["${y.replace(/ /g,'+')}"]%7D]&return_content=true`);
         return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
